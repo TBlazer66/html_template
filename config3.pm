3288e0ade9dd2b2edce646fd05ea353b31d27847
bob <tblazer66@gmail.com>
2017-06-09 15:34:04 -0700

    Add existing file

---
+++ config3.pm
@@ -0,0 +1,16 @@
+package config2;
+use Exporter	qw(import);
+our @EXPORT =	qw(%config);
+our %config = (
+     my_sftp =>  {
+           domain   =>  'value',
+           username =>  'value',
+           password =>  'value',
+          },
+my_fb =>  {
+        
+           email    =>  'value',
+           password =>  'value',
+          },
+     );
+1;
